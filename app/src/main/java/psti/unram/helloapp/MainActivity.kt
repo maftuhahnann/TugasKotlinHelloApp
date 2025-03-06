e46da6105376ee4680a1204c9610b02affba70ec
maftuhahnann <fadimaftuh28@gmail.com>
2025-03-06 13:20:57 +0800

    Tugas Kotlin

--- app/src/main/java/psti/unram/helloapp/MainActivity.kt
+++ app/src/main/java/psti/unram/helloapp/MainActivity.kt
@@ -1,20 +1,23 @@
 package psti.unram.helloapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edHello : EditText = findViewById(R.id.edHello)
+        val btnHello : Button = findViewById(R.id.btnHello)
+        val tvHello : TextView = findViewById(R.id.tvHello)
+
+        btnHello.setOnClickListener {
+            val str: String = edHello.text.toString()
+            tvHello.setText("Hello King $str")
         }
     }
 }
